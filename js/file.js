e8d9d774ebc688cfa4c38d75166a3aefd592e1e9
mattia0419 <pironemattia6@gmail.com>
2023-07-19 16:39:49 +0200

    add card

--- js/file.js
+++ js/file.js
@@ -5,11 +5,10 @@ let ticket_price = kmeters * 0.21 ;
 
 if(age < 18){
     ticket_price =ticket_price - ticket_price * 20 / 100 ;
-    console.log(ticket_price);
 }
 else if(age > 65){
     ticket_price =ticket_price - ticket_price * 40 / 100 ;
-    console.log(ticket_price);
 }
-const final_price = "Il biglietto costa" + " " + ticket_price.toFixed(2) + "€";
-document.getElementById("ticket_price-1").innerHTML = final_price
+const final_price = ticket_price.toFixed(2) + "€";
+const final_price_text = `${final_price}`
+document.getElementById("ticket_price-1").innerHTML = final_price_text
